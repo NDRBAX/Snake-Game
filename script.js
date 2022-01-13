ee58298135e87fa9e5de61333e6db6bd91b7b97f
NDRBAX-Fed <andre.bacaliu@icloud.com>
2022-01-13 20:41:19 +0100

    Does the snake eat the apple? Bug: the snake passes through the apple. Some esthetic corrections.

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ window.onload = function() {
     let widthInBlocks = canvasWidth / blockSize;
     let heightInBlocks = canvasHeight / blockSize;
     // Convert pixels to block
-
     init();
     // Call the function to run the initialization of the game
-
     function init() {
         let canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = '5px black solid';
+        canvas.style.border = '30px black solid';
+        canvas.style.margin = '50px auto';
+        canvas.style.backgroundColor = '#ddd';
+        canvas.style.display = 'block';
+
         document.body.appendChild(canvas);
         // Creation of the frame with the canvas method
         ctx = canvas.getContext('2d');
@@ -27,24 +29,31 @@ window.onload = function() {
             [6, 4],
             [5, 4],
             [4, 4],
+            [3, 4],
+            [2, 4],
         ], 'right');
         applee = new Apple([10, 10]);
         refreshCanvas();
-    }
-    // Function that allows you to create the movement, initialize the canvas
+    } // Function that allows you to create the movement, initialize the canvas
 
     function refreshCanvas() {
         snakee.advance();
         if (snakee.checkCollision()) {
             // GAME OVER
         } else {
+            if (snakee.isEatingApple(applee)) {
+                snakee.ateApple = true;
+                do {
+                    applee.setNewPosition();
+                    // The snake eat the apple
+                } while (applee.isOnSnake(snakee));
+            }
             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             snakee.draw();
             applee.draw();
             setTimeout(refreshCanvas, delay);
         }
-    }
-    // Set a time limit after which the initialization will take place
+    } // Set a time limit after which the initialization will take place
 
     function drawBlock(ctx, position) {
         let x = position[0] * blockSize;
@@ -55,6 +64,8 @@ window.onload = function() {
     function Snake(body, direction) {
         this.body = body;
         this.direction = direction;
+        this.ateApple = false;
+
         this.draw = function() {
             ctx.save();
             ctx.fillStyle = '#0f056b';
@@ -84,7 +95,10 @@ window.onload = function() {
                     break;
             }
             this.body.unshift(nextPosition);
-            this.body.pop();
+            if (!this.ateApple)
+                this.body.pop();
+            else
+                this.ateApple = false;
         };
 
         this.setDirection = function(newDirection) {
@@ -100,13 +114,12 @@ window.onload = function() {
                     break;
                 default:
                     throw ('Invalid direction');
-                    break;
-                    // Configuration of travel rules for the snake
-            }
+            } // Configuration of travel rules for the snake
             if (allowedDirections.indexOf(newDirection) > -1) {
                 this.direction = newDirection;
             } //Allow to change direction if the travel rules are respected
         };
+
         this.checkCollision = function() {
             let wallCollision = false;
             let snakeCollision = false;
@@ -121,58 +134,74 @@ window.onload = function() {
             let isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
             let isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
 
-            if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
+            if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls)
                 wallCollision = true;
-            }
 
             for (let i = 0; i < rest.length; i++) {
-                if (snakeX === rest[i][0] && snakeY === rest[i][1]) {
+                if (snakeX === rest[i][0] && snakeY === rest[i][1])
                     snakeCollision = true;
-                }
             }
             return wallCollision || snakeCollision;
+        };
 
+        this.isEatingApple = function(appleToEat) {
+            let head = this.body[0];
+            if (head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
+                return true;
+            else
+                return false;
         }
-
-
-    }
-    // Creation of the snake prototype function
+    } // Creation of the snake prototype function
 
     function Apple(position) {
         this.position = position;
+
         this.draw = function() {
             ctx.save(); // Permet de sauvegarder les anciennes configurations... 
             ctx.fillStyle = '#72bf08'; // ...puis d'appliquer le nouveau style... 
             ctx.beginPath();
             let radius = blockSize / 2;
-            let x = position[0] * blockSize + radius;
-            let y = position[1] * blockSize + radius;
+            let x = this.position[0] * blockSize + radius;
+            let y = this.position[1] * blockSize + radius;
             ctx.arc(x, y, radius, 0, Math.PI * 2, true);
             ctx.fill();
             ctx.restore(); // ...et enfin de restaurer les anciens paramètres. 
-        }
-    }
+        };
+        this.setNewPosition = function() {
+            let newX = Math.round(Math.random() * (widthInBlocks - 1));
+            let newY = Math.round(Math.random() * (heightInBlocks - 1));
+            this.positon = [newX, newY];
+        }; // Random creation of a new apple each time the apple is eaten
+        this.isOnSnake = function(snakeToCheck) {
+            let isOnSnake = false;
+            for (let i = 0; i < snakeToCheck.body.length; i++) {
+                if (this.positon[0] === snakeToCheck.body[i][0] && this.positon[1] === snakeToCheck.body[i][1]) {
+                    isOnSnake = true;
+                }
+            }
+            return isOnSnake;
+        };
+    } // Creation of the apple protoype function
 
     document.onkeydown = function handleKeyDown(e) {
-            let key = e.keyCode;
-            let newDirection;
-            switch (key) {
-                case 37:
-                    newDirection = 'left';
-                    break;
-                case 38:
-                    newDirection = 'up';
-                    break;
-                case 39:
-                    newDirection = 'right';
-                    break;
-                case 40:
-                    newDirection = 'down';
-                    break;
-                default:
-                    return;
-            }
-            snakee.setDirection(newDirection);
+        let key = e.keyCode;
+        let newDirection;
+        switch (key) {
+            case 37:
+                newDirection = 'left';
+                break;
+            case 38:
+                newDirection = 'up';
+                break;
+            case 39:
+                newDirection = 'right';
+                break;
+            case 40:
+                newDirection = 'down';
+                break;
+            default:
+                return;
         }
-        // Configure commands with key codes
+        snakee.setDirection(newDirection);
+    }; // Configure commands with key codes
 }
